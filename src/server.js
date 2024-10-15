3286ffe80680c92ddbe60b5aa3a12398e0a0124c
Andrei <chitoiu.andrei2@yahoo.com>
2024-10-15 15:33:26 +0300

    feat: added login and register routes with db save and search for validation

--- src/server.js
+++ src/server.js
@@ -24,6 +24,53 @@ mongodb.connect(process.env.MONGO_URI, {
       console.error('Error connecting to MongoDB: ' + err);
   });
 
+  app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+
+      const user = await User.findOne({ username });
+      console.log('User found:', user);
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+    
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+      const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      res.json({ token, message: 'Login successful!' });
+    } catch (error) {
+      console.error('Login error:', error); 
+      res.status(500).json({ message: 'Server error during login: ' + error.message });
+    }
+  });
+  
+  
+  app.post('/api/register', async (req, res) => {
+    const { username, password, email } = req.body;
+  
+    try {
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword, email });
+    await newUser.save();
+      res.status(201).json({ message: 'Registration successful!' });
+    } catch (error) {
+      console.error('Registration error:', error);
+      res.status(500).json({ message: 'Server error during registration: ' + error.message });
+    }
+  });
+  
+  
+  
+
+
 
 
 const port = 5000;
